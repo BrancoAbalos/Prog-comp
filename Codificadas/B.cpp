c0f31bf54b16d407cd25ec867203dd0ee7d87011
BrancoAbalos <branco.abalos@alumnos.ucn.cl>
2025-10-01 00:28:50 -0300

    B realizado

--- Codificadas/B.cpp
+++ Codificadas/B.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+    string k;
+    int t, c, s;
+    s = t = c = 0;
+    cin >> k;
     
+    for(char r:k) {
+        if(r == 'T') {
+        t++;
+        } else if (r == 'C') {
+        c++;
+        } else if (r == 'S') {
+        s++;
+        }
+    }
+    
+    if(s == t && s == c) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
 }
